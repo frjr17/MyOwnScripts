de00cd0bc7176d8bf0cf038b49904a5c0f7e9dfb
Hernán Valencia <hernanadrianv17@gmail.com>
2025-03-18 11:49:09 -0500

    🐛 fix: eliminating new line characters

--- favoriteShell.sh
+++ favoriteShell.sh
@@ -1,31 +1,31 @@
 # Step 1.) Update Everything
-echo '\n################################################'
+echo '################################################'
 echo 'Updating Everything'
-echo '################################################\n'
+echo '################################################'
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 
 # Step 2.) Installing zsh
-echo '\n################################################'
+echo '################################################'
 echo 'Installing ZSH'
-echo '################################################\n'
+echo '################################################'
 sudo apt-get install zsh -y
 
 # Step 3.) Installing Oh My Zsh (Unattended mode)
-echo '\n################################################'
+echo '################################################'
 echo 'Installing Oh my ZSH in unattended mode'
-echo '################################################\n'
+echo '################################################'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Step 4.) Installing Powerlevel10k files, but not initiating anything
-echo '\n################################################'
+echo '################################################'
 echo 'Installing Powerlevel10k files'
-echo '################################################\n'
+echo '################################################'
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Step 5.) Installing plugins
-echo '\n################################################'
+echo '################################################'
 echo 'Installing Powerlevel10k plugins'
-echo '################################################\n'
+echo '################################################'
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo apt install ruby-full
@@ -35,9 +35,9 @@ sudo gem install colorls
 sed -i 's/^plugins=.*/plugins=( git zsh-syntax-highlighting zsh-autosuggestions )/' ~/.zshrc
 
 # Step 6.) Setting up powerlevel10k as ZSH main theme
-echo '\n################################################'
+echo '################################################'
 echo 'Setting up Powerlevel 10k as ZSH main theme'
-echo '################################################\n'
+echo '################################################'
 sed -i 's|^ZSH_THEME=.*|ZSH_THEME=\"powerlevel10k/powerlevel10k\"|' ~/.zshrc
 
 # Step 7.) Adding custom aliases and colorls to ZSH
@@ -53,18 +53,18 @@ fi
 END
 
 # Step 8.) Setting up ZSH as default shell (password required) 
-echo '\n################################################'
+echo '################################################'
 echo 'Setting up ZSH as default shell'
-echo '################################################\n'
+echo '################################################'
 echo 'Please enter your password below'
 chsh -s /bin/zsh
 zsh
 
 
 # Step 9.) Changing powerlevel10k shorten strategy to truncate_to_last 
-echo '\n################################################'
+echo '################################################'
 echo 'Changing powerlevel10k shorten strategy to truncate_to_last'
-echo '################################################\n'
+echo '################################################'
 sed -i 's|typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique|typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last|' ~/.p10k.zsh
 
 # Done
