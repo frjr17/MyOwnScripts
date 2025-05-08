45abd16b9794e13798cb9d14a6840f3fd1c56c6e
Hernán Valencia <hernanadrianv17@gmail.com>
2025-05-07 23:59:26 -0500

    ✨ feat: adding new script for configuring my fedora workstation

---
+++ installEverything.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# MacOS Theme
+
+# UI apps 
+#   VS Code
+#   Spotify
+#   Notion
+#   Notion Calendar
+#   Google Chrome
+#   Telegram
+#   Variety
+
+# Install dev fonts
+
+# Install powerlevel10k theme for terminal
+
