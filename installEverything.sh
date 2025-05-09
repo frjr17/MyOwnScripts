af9434dad49be5ba4ae04c53d7e32e5e6a6ce022
Hernan Valencia <hernanadrianv17@gmail.com>
2025-05-08 23:33:26 -0500

    ✨ feat: finishing softwares installation

--- installEverything.sh
+++ installEverything.sh
@@ -31,9 +31,9 @@ mv *.otf /usr/share/fonts # Operator Mono
 # Install powerlevel10k theme for terminal
 sudo dnf install zsh curl -y
 
-sudo dnf install ruby 
-sudo dnf install rubygem-{irb,rake,rbs,rexml,typeprof,test-unit} ruby-bundled-gems
-sudo dnf install make automake gcc gcc-c++ kernel-devel
+sudo dnf install ruby -y
+sudo dnf install rubygem-{irb,rake,rbs,rexml,typeprof,test-unit} ruby-bundled-gems -y
+sudo dnf install make automake gcc gcc-c++ kernel-devel -y
 sudo gem install colorls
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -45,24 +45,40 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 cd
 echo "alias update=sudo dnf update && sudo dnf upgrade && sudo dnf autoremove" >> .zshrc
 echo "alias rmdir=rm -rf" >> .zshrc
-echo "alias open=xdg-open" 
-
+echo "alias open=xdg-open" >> .zshrc
+echo "alias python=python3" >> .zshrc
+echo "alias venv_activate=source ./venv/bin/activate" >> .zshrc
+echo "alias create_venv=python -m venv venv" >> .zshrc
 
 # UI apps 
+sudo ln -s /var/lib/snapd/snap /snap
 #   VS Code
+sudo snap install code --classic
 #   Spotify
-#   Notion
-#   Notion Calendar
+sudo snap install spotify
 #   Google Chrome
-#   Telegram
-#   Variety
-#   Whatsapp
+sudo snap install google-chrome
 
+#   Telegram
+sudo snap install telegram-desktop
+#   Variety
+sudo dnf install variety
 
 # Install Development softwares
 #   Node.js (NVM)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install --lts
+
 #   Python
+sudo dnf install python3-pip -y
 #   Go
-#   Java
+sudo dnf install golang -y 
+mkdir -p $HOME/go
+echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
+source $HOME/.bashrc
+#   Java 21 
+sudo dnf install java-21-openjdk -y
 #   Vim
+sudo dnf install vim-enhanced -y
 
