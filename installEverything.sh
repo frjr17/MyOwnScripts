ee329b5da7ac2321316e4f8502a5ca95ee4ba6b0
Hernan Valencia <hernanadrianv17@gmail.com>
2025-05-08 23:06:09 -0500

    ✨ feat: adding common aliases

--- installEverything.sh
+++ installEverything.sh
@@ -35,15 +35,17 @@ sudo dnf install ruby
 sudo dnf install rubygem-{irb,rake,rbs,rexml,typeprof,test-unit} ruby-bundled-gems
 sudo dnf install make automake gcc gcc-c++ kernel-devel
 sudo gem install colorls
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-#   Common aliases
-
+# Common aliases
+cd
+echo "alias update=sudo dnf update && sudo dnf upgrade && sudo dnf autoremove" >> .zshrc
+echo "alias rmdir=rm -rf" >> .zshrc
+echo "alias open=xdg-open" 
 
 
 # UI apps 
