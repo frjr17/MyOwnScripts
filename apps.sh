5ffacb96f7014dabee8e8cbef4ce9fb7042f9938
Hernán Valencia <45920897+frjr17@users.noreply.github.com>
2025-05-27 18:08:22 +0000

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo dnf install snapd -y
 sudo ln -s /var/lib/snapd/snap /snap
-#   VS Code
-sudo snap install code --classic
+
 #   Spotify
 sudo snap install spotify
 #   Google Chrome
@@ -11,3 +10,5 @@ sudo dnf install google-chrome-stable -y
 sudo snap install telegram-desktop
 #   Variety
 sudo dnf install variety -y
+#   VS Code
+sudo snap install code --classic
