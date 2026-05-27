f33cbf1da3fa784625a40664bd14c9dbbf4aad31
Hernán Valencia <hernanadrianv17@gmail.com>
2026-05-26 20:20:50 -0500

    ♻️ refactor: modifying custom shorcuts

--- fedoraSetup.sh
+++ fedoraSetup.sh
@@ -27,16 +27,13 @@ gsettings set org.gnome.shell.keybindings show-applications "['<Super>a']"
 # Notifications
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>n']"
 
-# Settings - open via dbus/Activities
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "[]"
-dbus-launch gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'Sundry']" 2>/dev/null || true
 
 # Settings
 gsettings set org.gnome.shell.keybindings open-application-grid "['<Super>i']"
 
 # Custom shortcuts
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/nautilus-open/name "'Open Files'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/nautilus-open/command "'nautilus'"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/nautilus-open/binding "'<Super>e'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Open File Explorer'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'nautilus'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>e'
 
 echo "✅ Keyboard shortcuts configured!"
